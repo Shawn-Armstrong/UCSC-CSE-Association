90c9387f587905617fc5c21acd1276f942de9738
Shawn Armstrong <smarmstr@ucsc.edu>
2023-11-29 20:39:57 -0800

    Update database schema to support register verification

--- database/init.sql
+++ database/init.sql
@@ -3,13 +3,17 @@ CREATE TABLE IF NOT EXISTS my_table (
   name VARCHAR(255) NOT NULL
 );
 
+-- Updated users table
 CREATE TABLE IF NOT EXISTS users (
   id SERIAL PRIMARY KEY,
   username VARCHAR(255) UNIQUE NOT NULL,
   email VARCHAR(255) UNIQUE NOT NULL,
   password_hash VARCHAR(255) NOT NULL,
+  is_verified BOOLEAN DEFAULT FALSE, -- Default to false until the user verifies their email
+  verification_token VARCHAR(255), -- Stores the email verification token
   created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
   updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
 
+-- Existing sample data insertion
 INSERT INTO my_table (name) VALUES ('Sample Data');
