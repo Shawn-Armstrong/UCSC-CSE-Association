0eb6d716b4b537e5ffceb8cdef06583b9833e58b
Shawn Armstrong <smarmstr@ucsc.edu>
2024-01-04 00:32:42 -0800

    Center subject content for home view

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,64 +1,75 @@
 <template>
-  <v-container class="mt-12">
-    <v-img
-      contain
-      max-height="100px"
-      max-width="auto"
-      src="../assets/ucsc_logo.png"
-      class="my-10"
-    ></v-img>
+  <v-container fluid>
+    <v-row
+      justify="center"
+      align="center"
+      class="mx-3 my-0"
+      style="height: calc(100vh - 64px - 64px)"
+    >
+      <v-col cols="12" class="pa-0 d-flex flex-column justify-space-between">
+        <v-img
+          contain
+          max-height="100px"
+          max-width="auto"
+          src="../assets/ucsc_logo.png"
+          class="mb-4"
+        ></v-img>
 
-    <video-background
-      :src="introVideo"
-      style="max-height: 300px; height: 100vh"
-    ></video-background>
-    <h1 class="text-center my-2" style="color: #003c6c">
-      Computer Science and Engineering Mentoring
-    </h1>
+        <video-background
+          :src="introVideo"
+          :sources="[
+            { src: 'src/assets/intro.mp4', res: 991, autoplay: true },
+            { src: 'src/assets/intro.mp4', res: 575, autoplay: true },
+          ]"
+          style="max-height: 300px; height: 100vh"
+        ></video-background>
+        <h1 class="text-center my-2" style="color: #003c6c">
+          Computer Science and Engineering Mentoring
+        </h1>
 
-    <v-row class="my-5" justify="center">
-      <v-btn
-        color="#1b77d2"
-        class="text-white mx-10 my-2"
-        prepend-icon="mdi-login"
-        elevation="12"
-        raised
-        size="large"
-        @click="navigateToLogin"
-      >
-        Login
-      </v-btn>
-      <v-btn
-        color="#1b77d2"
-        class="text-white mx-10 my-2"
-        prepend-icon="mdi-account-plus-outline"
-        elevation="12"
-        raised
-        size="large"
-        @click="navigateToRegister"
-      >
-        Register
-      </v-btn>
+        <v-row class="my-5" justify="center">
+          <v-btn
+            color="#1b77d2"
+            class="text-white mx-10 mt-2 mb-12"
+            prepend-icon="mdi-login"
+            elevation="12"
+            raised
+            size="large"
+            @click="navigateToLogin"
+          >
+            Login
+          </v-btn>
+          <v-btn
+            color="#1b77d2"
+            class="text-white mx-10 mt-2 mb-12"
+            prepend-icon="mdi-account-plus-outline"
+            elevation="12"
+            raised
+            size="large"
+            @click="navigateToRegister"
+          >
+            Register
+          </v-btn>
+        </v-row>
+      </v-col>
     </v-row>
   </v-container>
 </template>
 
 <script setup>
-import { useRouter } from 'vue-router';
-import introVideo from '@/assets/intro.mp4';
-import VideoBackground from 'vue-responsive-video-background-player';
+import { useRouter } from "vue-router";
+import introVideo from "@/assets/intro.mp4";
+import VideoBackground from "vue-responsive-video-background-player";
 
 const router = useRouter();
 
 const navigateToLogin = () => {
-  router.push('/login');
+  router.push("/login");
 };
 
 const navigateToRegister = () => {
-  router.push('/register');
+  router.push("/register");
 };
 </script>
 
-<style>
-/* Add any additional styling you want here */
-</style>
+<style></style>
